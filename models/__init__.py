6f74e535845bbf76a9ad58a3e101e67b36e9e20f
Reno Kriz <rekriz@seas.upenn.edu>
2019-02-23 16:14:56 -0500

    Debug

--- models/__init__.py
+++ models/__init__.py
@@ -12,6 +12,7 @@ from .Att2inModel import Att2inModel
 from .AttModel import *
 
 def setup(opt):
+    print("Number of clusters: " + str(opt.num_clusters))
     
     if opt.caption_model == 'show_tell':
         model = ShowTellModel(opt)
